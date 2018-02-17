8ce9d4c39be193b2f26646885eac8ede644473f6
Elisia Morgan <elisiamorgan@quickenloans.com>
2018-02-17 17:02:04 -0500

    updated versions

--- JQ labs/lab1.js
+++ JQ labs/lab1.js
@@ -1,4 +1,78 @@
- $(".tables").click(function(){
-        $(this).fadeOut('slow');
-    });
-$('div.container').replaceWith('new paragraph');
+.titles{
+  text-align: center;
+  font-size: 25px
+  
+}
+
+/*.tables{*/
+/*  text */
+/*}*/
+
+/*.buttons {*/
+/*  border-radius: 100%;*/
+/*  width: 80px;*/
+/*  height: 80px;*/
+  
+/*}*/
+
+.container {
+  border: 1px solid red;
+  max-width: 500px;
+  margin: auto;
+}
+
+.row {
+  display: flex;
+  height:150px;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.row div {
+  display: flex;
+  height: 100px;
+  width: 100px;
+  border: 2px solid;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  text-align: center;
+  border-radius: 50%;
+}
+
+.formContainer{
+  align-content: center;
+  
+}
+
+
+.form {
+  background-color: #ffffff;
+  top: 100px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  height: 300px;
+  text-align: center;
+  
+}
+.form p{
+  color: red;
+  text: bold;
+}
+/*#close:before {*/
+/*  content: "✖";*/
+/*  border: 1px solid gray;*/
+/*  background-color:#eee;*/
+/*  padding:0.5em;*/
+/*  cursor: pointer;*/
+  
+/*}*/
+
+.hidden{
+  display: none;
+}
+
+
