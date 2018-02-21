899f742adac477846ee32bbad15d50a7404d75ef
Elisia Morgan <elisiamorgan@quickenloans.com>
2018-02-20 19:29:18 -0500

    corrected javascript and css

--- JQ labs/lab1.js
+++ JQ labs/lab1.js
@@ -1,78 +1,16 @@
-.titles{
-  text-align: center;
-  font-size: 25px
-  
-}
+var $form = document.getElementById('form');
+// var $close = documnent.getElementById('close');
 
-/*.tables{*/
-/*  text */
-/*}*/
+$('#1, #2, #3, #4, #5, #6, #7, #8, #9').click(function(){
+  $('.form').show();
+});
 
-/*.buttons {*/
-/*  border-radius: 100%;*/
-/*  width: 80px;*/
-/*  height: 80px;*/
-  
-/*}*/
+document.getElementById('close').addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentNode.style.display = 'none';
+}, false);
 
-.container {
-  border: 1px solid red;
-  max-width: 500px;
-  margin: auto;
-}
-
-.row {
-  display: flex;
-  height:150px;
-  align-items: center;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-
-.row div {
-  display: flex;
-  height: 100px;
-  width: 100px;
-  border: 2px solid;
-  justify-content: center;
-  align-content: center;
-  flex-direction: column;
-  text-align: center;
-  border-radius: 50%;
-}
-
-.formContainer{
-  align-content: center;
-  
-}
-
-
-.form {
-  background-color: #ffffff;
-  top: 100px;
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  width: 350px;
-  height: 300px;
-  text-align: center;
-  
-}
-.form p{
-  color: red;
-  text: bold;
-}
-/*#close:before {*/
-/*  content: "✖";*/
-/*  border: 1px solid gray;*/
-/*  background-color:#eee;*/
-/*  padding:0.5em;*/
-/*  cursor: pointer;*/
-  
-/*}*/
-
-.hidden{
-  display: none;
-}
-
-
+$('.submit').on('click', function() {
+    var numberVal = $('number').val(); 
+    console.log('numberVal');
+});
